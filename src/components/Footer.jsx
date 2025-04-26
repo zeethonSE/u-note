74d00c47a604ab49d9649d498d03801506e5eb95
zeethonSE <zeethon0@gmail.com>
2025-04-26 20:51:48 +0700

    Update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 function Footer() {
-  const year = new Date().getFullYear();
+ 
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>Copyright ⓒ {new Date().getFullYear()} Sai Sai uNote</p>
     </footer>
   );
 }
